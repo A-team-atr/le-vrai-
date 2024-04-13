7b1afa311ca01303dda4a830f375b9ec0cc84bcb
A-team-atr <raphael.atieh@bluewin.ch>
2024-04-13 13:54:10 +0200

    maison

---
+++ echelle.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+
+
+
+func _on_body_entered(body):
+	if body.name == "CharacterBody2D":
+		get_node("/root/main/CharacterBody2D").echelle_active = true 
+
+
+func _on_body_exited(body):
+	if body.name == "CharacterBody2D":
+		get_node("/root/main/CharacterBody2D").echelle_active = false 
