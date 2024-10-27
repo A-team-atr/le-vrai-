981fb5e9037d195e6490b4e323e07936cbf01da0
A-team-atr <raphael.atieh@bluewin.ch>
2024-10-27 13:02:23 +0100

    27.10.2024

--- pecheur_jouer.gd
+++ pecheur_jouer.gd
@@ -1,12 +1,12 @@
 extends CharacterBody2D
 
-@onready var anim = get_node("anim_joueur_peche")
+@onready var anim = $AnimationPlayer
 var paused = false 
 
 
 func _ready():
 	anim.play("repos")
-	anim.flip_h = true
+	$repos.flip_h = true
 
 func _process(delta):
 	if Input.is_action_just_pressed("pause"):
@@ -17,4 +17,9 @@ func _process(delta):
 func _physics_process(delta):
 	if Input.is_action_pressed("interagir"):
 		anim.play("lancer")
-		
+		$lancer.flip_h = true
+
+
+func _on_anim_joueur_peche_animation_finished():
+	if anim == "lancer":
+		anim.play("entrain_de_pêcher")
